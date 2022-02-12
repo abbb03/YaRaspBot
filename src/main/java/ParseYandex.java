a76e3da3cff5a99c73473cd524d6467bd2937a18
ARBUZZZ228 <alecksey17@icloud.com>
2022-02-12 21:00:20 +0300

    init: start yaraspbot-task

---
+++ src/main/java/ParseYandex.java
@@ -0,0 +1,26 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ParseYandex {
+    public static String json;
+
+    public static String parse(){
+        StringBuilder stringBuilder = new StringBuilder();
+        JSONObject obj = new JSONObject(json);
+
+        String from = obj.getJSONObject("search").getJSONObject("from").getString("title");
+        String to = obj.getJSONObject("search").getJSONObject("to").getString("title");
+        String direction = "Из " + from + " в " + to + "\n";
+
+        stringBuilder.append(direction);
+
+        JSONArray arr = obj.getJSONArray("segments");
+        for (int i = 0; i < arr.length(); i++) {
+            String departure = arr.getJSONObject(i).getString("departure").substring(0, 5);
+            String arrival = arr.getJSONObject(i).getString("arrival").substring(0, 5);
+            String route = departure + " - " + arrival + "\n";
+            stringBuilder.append(route);
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ src/main/java/YaRaspBot.java
@@ -0,0 +1,105 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class YaRaspBot extends TelegramLongPollingBot {
+
+    static String furmanov = "c20682";
+    static String ivanovo = "s9839568";
+
+    private boolean checkText(Update update) {
+        return update.hasMessage() && update.getMessage().hasText();
+    }
+
+    private synchronized void sendMsg(String chatId, String s) throws IOException {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        if (s.equals("1")) {
+            String request = "https://api.rasp.yandex.net/v3.0/search/?apikey=" + Config.yandexToken + Config.format +
+                     "&from=" + furmanov + "&to=" + ivanovo + "&transport_types=bus&lang=ru_RU&page=1";
+            ParseYandex.json = getResponse(request);
+            sendMessage.setText(ParseYandex.parse());
+        }
+        else if (s.equals("2")) {
+            String request = "https://api.rasp.yandex.net/v3.0/search/?apikey=" + Config.yandexToken + Config.format +
+                    "&to=" + furmanov + "&from=" + ivanovo + "&transport_types=bus&lang=ru_RU&page=1";
+            ParseYandex.json = getResponse(request);
+            sendMessage.setText(ParseYandex.parse());
+        }
+        else
+        {
+            sendMessage.setText("Шёл бы ты отсюда...");
+        }
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static HttpURLConnection getConnection(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setConnectTimeout(500);
+        connection.setReadTimeout(500);
+        connection.connect();
+        return connection;
+    }
+
+    public static String getResponse(String request) throws IOException {
+        HttpURLConnection connection = null;
+        try {
+            final URL url = new URL(request);
+            connection = getConnection(url);
+            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                StringBuilder stringBuilder = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    stringBuilder.append(inputLine);
+                }
+
+                return stringBuilder.toString();
+            } else {
+                System.out.println("fail" + connection.getResponseCode() + ", " + connection.getResponseMessage());
+                return null;
+            }
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (checkText(update)) {
+            String chatId = update.getMessage().getChatId().toString();
+            String s = update.getMessage().getText();
+            try {
+                sendMsg(chatId, s);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "YaRaspBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return Config.telegramToken;
+    }
+}
